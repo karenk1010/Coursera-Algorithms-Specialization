8534678f6e983f2aad4d32de348262f2fafd8274
karenk1010 <karen_kirakosyan@yahoo.com>
2020-07-09 00:23:34 -0700

    placeholder for solutions

---
+++ Course-3:Greedy-Algorithms/knapsack1.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+# Problem Statement #1 {{{
+"""
+In this programming problem and the next you'll code up the knapsack algorithm from lecture.
+Download the text file below (knapsack1.txt).
+
+This file describes a knapsack instance, and it has the following format:
+[knapsack_size][number_of_items]
+[value_1] [weight_1]
+[value_2] [weight_2]
+...
+
+For example, the third line of the file is "50074 659", indicating that the
+second item has value 50074 and size 659, respectively.
+
+You can assume that all numbers are positive. You should assume that item weights
+and the knapsack capacity are integers.
+
+In the box below, type in the value of the optimal solution.
+
+ADVICE: If you're not getting the correct answer, try debugging your algorithm using
+some small test cases. And then post them to the discussion forum!
+
+- problem#1 answer: 2493893.0
+"""
+# }}}
+
+# vim:foldmethod=marker:foldlevel=0
