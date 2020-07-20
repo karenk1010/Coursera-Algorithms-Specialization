5cdda2f69a74e72cbc97c24d46a90a0debdb6838
karenk1010 <karen_kirakosyan@yahoo.com>
2020-07-20 00:07:03 -0700

    minor updates

--- Course-4:Shortest-Path-Revisited/tsp.py
+++ Course-4:Shortest-Path-Revisited/tsp.py
@@ -26,7 +26,7 @@ HINT: You might experiment with ways to reduce the data set size.
 For example, trying plotting the points. Can you infer any structure
 of the optimal solution? Can you use that structure to speed up your algorithm?
 
-- problem answer: 
+- problem answer: 26442
 """
 # }}}
 
